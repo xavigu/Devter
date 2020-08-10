9ad581620b0390972fe69f98a7bc623ef19e30ef
xavigu <xavigu91@gmail.com>
2020-08-10 18:03:25 +0200

    Use hello api to fetch the username in Timeline page

--- pages/timeline/index.js
+++ pages/timeline/index.js
@@ -19,7 +19,11 @@ export default function Timeline({ userName }) {
 }
 
 Timeline.getInitialProps = () => {
-  return {
-    userName: 'Javi'
-  }
+  return fetch('http://localhost:3000/api/hello')
+   .then(res => res.json())
+   .then(response => {
+     console.log(response);
+     const {userName} = response;
+     return {userName};
+   })
 }
