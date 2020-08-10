8249394428008738919c490dc06993965767478f
xavigu <xavigu91@gmail.com>
2020-08-10 17:53:54 +0200

    Add some global styles in _app.js file

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  return (
+  <>
+  <Component {...pageProps}/>
+  <style jsx global>{`
+    html,
+    body {
+      padding: 0;
+      margin: 0;
+      font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+        Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    }
+  `}
+  </style>
+  </>
+  )
 }
-
-export default MyApp
